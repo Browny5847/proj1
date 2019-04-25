95486481dcc43dc4173dd70fc25f69bdcf34dc2a
Zac Brown <browny5847@hotmail.com>
2019-04-25 09:13:58 +0000

    Main function

--- substitutionCipher.c
+++ substitutionCipher.c
@@ -1,21 +1,31 @@
 #include<stdio.h> 
 
-int main(){
+int main()
+{
 
-int i;
-
-char arr1[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N',
-                'O','P','Q','R','S','T','U','V','W','X','W','Z'};
-char arr2[] = {'Q','W','E'};
-
-for (i = 0; i < 100 && arr1[i] != '\0'; i++)
-    arr1[i] = arr2[i];
- 
-printf("%c", arr1[i]);
-printf("%c", arr2[i]);
-
-//\nEncrypted message: %s\n", message)
-
-
-return 0; 
+    int i;
+    int x;
+    char message[100];
+    
+    //char arr1[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N',
+                    // 'O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+    char arr[] = {'Q','W','E','A','B','C','D','F','G','H','I','J','K','L','M','N',
+                    'O','P','R','S','T','U','V','W','X','Z'};
+    
+    printf("\nPlease enter the message: ");
+    scanf("%s", &message);
+    
+       for (i = 0; (i < 100 && message[i] != '\0'); i++)
+       {
+          if (message[i] >= 65 && message[i] <= 90)
+    	  {
+    	    x = message[i] - 65;
+    	  }
+    	  else if (message[i] >= 97 && message[i] <= 122)
+    	  {
+    	    x = message[i] - 97;
+    	  }
+          printf("\nEncrypted Message: %c", arr[x]);
+        } 
+    return 0; 
 }
