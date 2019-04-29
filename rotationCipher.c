7a45b70b81d481ad2f9d48b774bef73361e4dfd0
Zac Brown <browny5847@hotmail.com>
2019-04-29 06:16:48 +0000

    Comments and indentation

--- rotationCipher.c
+++ rotationCipher.c
@@ -3,15 +3,16 @@
 int
 main ()
 {
-  int x; 
+  int x;
   int i;
-  int g; // the key for when unknown
-  int k; //The Key
-  char message[100]; //Message input
-  char alpha[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N',  // Alphabet Array
-                'O','P','Q','R','S','T','U','V','W','X','Y','Z'};
-    
-  
+  int g;			// the key for when unknown
+  int k;			//The Key
+  char message[100];		//Message input
+  char alpha[] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',	// Alphabet Array
+    'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+  };
+
+
 
 // Scanning for the message to be encrypted and the key vaule.
 
@@ -35,14 +36,14 @@ main ()
 
   switch (x)
     {
-    // case 1 is Encryption to all uppercase
+      // case 1 is Encryption to all uppercase
     case 1:
       for (i = 0; (i < 100 && message[i] != '\0'); i++)
-	if (message[i] >= 65 && message[i] <= 90) // Uppercase Decryption
+	if (message[i] >= 65 && message[i] <= 90)	// Uppercase Decryption
 	  {
-	    message[i] = ((message[i] - 65) + k) % 26 + 65; 
+	    message[i] = ((message[i] - 65) + k) % 26 + 65;
 	  }
-	else if (message[i] >= 97 && message[i] <= 122) // Lowercase Decryption
+	else if (message[i] >= 97 && message[i] <= 122)	// Lowercase Decryption
 	  {
 	    message[i] = ((message[i] - 97) + k) % 26 + 65;
 	  }
@@ -50,29 +51,29 @@ main ()
       break;
 
     case 2:
-    // case 2 is Decryption to all uppercase
+      // case 2 is Decryption to all uppercase
       for (i = 0; (i < 100 && message[i] != '\0'); i++)
-	if (message[i] >= 65 && message[i] <= 90) // Uppercase Decryption
+	if (message[i] >= 65 && message[i] <= 90)	// Uppercase Decryption
 	  {
 	    message[i] = ((message[i] - 65) - k + 26) % 26 + 65;
 	  }
-	else if (message[i] >= 97 && message[i] <= 122) // Lowercase Decryption
+	else if (message[i] >= 97 && message[i] <= 122)	// Lowercase Decryption
 	  {
 	    message[i] = ((message[i] - 97) - k + 26) % 26 + 97;
 	  }
       printf ("\nDecrypted message: %s\n", message);
       break;
-      
+
     case 3:
-    // case 3 is Decryption with unknown key
-      g = abs((alpha[0] - 65) - (message[0] - 65));
-      printf("Key is: %d", g);
-      for (i = 0; (i < 100 && message[i] != '\0'); i++)  
-      if (message[i] >= 65 && message[i] <= 90) // Uppercase Decryption
+      // case 3 is Decryption with unknown key
+      g = abs ((alpha[0] - 65) - (message[0] - 65));
+      printf ("Key is: %d", g);
+      for (i = 0; (i < 100 && message[i] != '\0'); i++)
+	if (message[i] >= 65 && message[i] <= 90)	// Uppercase Decryption
 	  {
 	    message[i] = ((message[i] - 65) - g + 26) % 26 + 65;
 	  }
-	else if (message[i] >= 97 && message[i] <= 122) // Lowercase Decryption
+	else if (message[i] >= 97 && message[i] <= 122)	// Lowercase Decryption
 	  {
 	    message[i] = ((message[i] - 97) - g + 26) % 26 + 97;
 	  }
